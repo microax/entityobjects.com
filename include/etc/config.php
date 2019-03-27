f20f285ccaed2af2892fcc270f6ca304805114e4
microax <mgill@metaqueue.net>
2019-03-27 19:11:58 -0400

    refactor navigation

--- include/etc/config.php
+++ include/etc/config.php
@@ -5,14 +5,39 @@
  ***********************************
 */			
 
+function systemEmail()
+{
+    $admins = array('mgill@metaqueue.net');
+    return($admins);
+}
+
 function pubServerAddress()
 {
-    return("http://temp.metaq");
+    return("http://entityobjects.com");
 }
 
 function smtpConfig()
 {
     return("/opt/install/PHPMailer/PHPMailer.json");
+}
+
+function configValidPhotoExtensions()
+{
+    $validExtensions =  ['jpg', 'jpeg', 'png', 'gif'];
+    return($validExtensions);
+}
+
+function configValidMediaExtensions()
+{
+    $validExtensions = ['mp3', 'mp4a'];
+    return($validExtensions);
+}
+
+function configMaxUploadSize()
+{
+    // I'm not sure how I got this number LOL
+    //---------------------------------------
+    return(2097152);
 }
 
 ?>
